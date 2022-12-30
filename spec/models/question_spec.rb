232c7f2abaf8573c2ed827acb624351e40743a6e
Kester Dobson <kdobson@gmail.com>
2022-12-30 08:57:34 +0000

    Formatting fix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -16,7 +16,9 @@ describe 'QuestionTest' do
     describe 'when we try and create the same question' do
       it 'fails to create' do
         duplicated_question = Question.new(subject.values.slice(:user_id, :body, :title))
-        expect { duplicated_question.save }.to raise_error(an_instance_of(Sequel::UniqueConstraintViolation).and having_attributes(message: "SQLite3::ConstraintException: UNIQUE constraint failed: questions.user_id, questions.title"))
+        expect do
+          duplicated_question.save
+        end.to raise_error(an_instance_of(Sequel::UniqueConstraintViolation).and(having_attributes(message: 'SQLite3::ConstraintException: UNIQUE constraint failed: questions.user_id, questions.title')))
       end
     end
   end
